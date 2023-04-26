f8faf5c29551dad47fb457424d709edded362c8d
Samm Lopes <sam.v.p.lopes@gmail.com>
2023-04-26 09:31:07 -0300

    Inserido os metodos getters e setters na classe Jogador e adicionado o hashcode e equals da classe carta

--- src/com/blackjack/models/Carta.java
+++ src/com/blackjack/models/Carta.java
@@ -2,8 +2,9 @@ package com.blackjack.models;
 
 public class Carta {
 
-    //Atributos da carta
-    String nipe;
+   
+   //Atributos da carta
+   String nipe;
     String face;
     int valor;
 
@@ -38,5 +39,38 @@ public class Carta {
          this.valor = valor;
      }
      
+     @Override
+     public int hashCode() {
+       final int prime = 31;
+       int result = 1;
+       result = prime * result + ((nipe == null) ? 0 : nipe.hashCode());
+       result = prime * result + ((face == null) ? 0 : face.hashCode());
+       result = prime * result + valor;
+       return result;
+     }
+     
+     @Override
+     public boolean equals(Object obj) {
+       if (this == obj)
+          return true;
+       if (obj == null)
+          return false;
+       if (getClass() != obj.getClass())
+          return false;
+       Carta other = (Carta) obj;
+       if (nipe == null) {
+          if (other.nipe != null)
+             return false;
+       } else if (!nipe.equals(other.nipe))
+          return false;
+       if (face == null) {
+          if (other.face != null)
+             return false;
+       } else if (!face.equals(other.face))
+          return false;
+       if (valor != other.valor)
+          return false;
+       return true;
+     }
 }
 
--- src/com/blackjack/models/Jogador.java
+++ src/com/blackjack/models/Jogador.java
@@ -17,6 +17,44 @@ public class Jogador{
         this.pontos = 0;
     }
 
+    //Metodos getters e setters
+    //getters
+    public List<Carta> getMao(){
+        return this.mao;
+    }
 
+    public Carta getCartaMao(Carta c){
+        for(Carta carta : mao){
+            if(carta.equals(c)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public List<Carta> getCartasGanhas(){
+        return this.cartasGanhas;
+    }
+
+    public int getPontos(){
+        return this.pontos;
+    }
+
+    //setters
+    public void setMao(List<Carta> cartas){
+        this.mao = cartas;
+    }
+
+    public void setCartasMao(Carta c){
+        this.mao.add(c);
+    }
+
+    public void setCartasGanhas(List<Carta> cartas){
+        this.cartasGanhas = cartas;
+    }
+
+    public void setPontos(int pontos){
+        this.pontos = pontos;
+    }
 
 }
