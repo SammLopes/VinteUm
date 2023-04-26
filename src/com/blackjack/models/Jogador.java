f56a05246597cd50eaf4183e7dd5622447103c6c
Samm Lopes <sam.v.p.lopes@gmail.com>
2023-04-26 09:12:31 -0300

    Adicionado os atributos da classe jogador e o metodo construtor

--- src/com/blackjack/models/Jogador.java
+++ src/com/blackjack/models/Jogador.java
@@ -1,5 +1,22 @@
 package com.blackjack.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jogador{
+
+    //atributos
+    private List<Carta> mao ;
+    private List<Carta> cartasGanhas ;
+    private int pontos;
+
+    //Metodo construtor
+    public Jogador(){
+        this.mao = new ArrayList<>();
+        this.cartasGanhas = new ArrayList<>();
+        this.pontos = 0;
+    }
+
+
 
 }
